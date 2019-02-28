abcd02e4b05a5151c226202651605ac75dee8b9b
David Sarkisian <dsarkisian@plansource.com>
2019-02-27 19:07:02 -0700

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #controller 
+
+  root 'subs#index'
+
+  resources :subs do
+    resources :topics
+  end
+  
+
+  # resources :subs, only: [:index, :show]
+  # get '/subs', to: 'subs#index'
+  # get '/sub/:id', to: 'subs#show'
+  # get '/sub/:id', to: 'subs#edit'
+  # put '/sub/:id', to: 'subs#update'
+  # delete '/sub/:id', to: 'subs#destroy'
 end
